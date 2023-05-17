8b2e7216f5b9c7298adc849dd0590206a3cbb1e5
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-05-16 18:09:57 -0700

    changes to shell.h

--- shell.h
+++ shell.h
@@ -5,18 +5,49 @@
 #define ERROR_CHARACTER  (0)
 #define SOURCE_INIT_POINT (-2)
 
-struct new_source
-{
-	char *boff;
-	long boffsize;
-	long new_post;
-};
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <error.h>
 #include <string.h>
 #include <unistd.h>
+
+/**
+ * struct new_source - source.
+ *
+ * @boof: buffer.
+ * @boffsize: buffer size.
+ * @new_post: new nude.
+ */
+struct new_source
+{
+        char *boff;
+        long boffsize;
+        long new_post;
+};
+
+/**
+ * struct toks - represent end of file.
+ *
+ * @input_len: lenght of input.
+ * @input. user input.
+ */
+struct toks
+{
+	struct new_source *source;
+	int input_len;
+	char *input;
+};
+
+/**
+ * eof_tok - represent the end of file.
+ *
+ * - input_len: struct variable.
+ */
+struct toks eof_tok = {
+        .input_len = 0,
+};
+
+extern struct toks eof_tok;
 
 /** STRING PROTOTYPE **/
 int strcmp(const char *str1, const char *str2);
@@ -33,4 +64,8 @@ void *realloc(void *pointer, size_t size);
 void *memcpy(void *dest, const void *src, size_t n);
 
 void get_last_char(struct new_source *source);
+
+/** Tokenizing Input **/
+struct toks *sep(struct source_s *source);
+void free_all_toks(struct toks *token);
 #endif
