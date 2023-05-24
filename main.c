a684a406678cddd9ffbebe5e758e6527d42d55ee
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-05-24 06:53:08 -0700

    changes

--- main.c
+++ main.c
@@ -1,50 +1,20 @@
 #include "shell.h"
 
 /**
- * main - defining a function.
- * @argc: argument.
- * @argv: argument.
+ * main - function that checks if our shell is called
  *
- * Return: value
+ * Return: 0 on success
  */
-int main(int argc, char **argv)
+int main(void)
 {
-	struct new_source source;
-	char *input;
-	(void)argc;
-	(void)argv;
-
-	while (1)
+	/* determines if file descriptor is associated with a terminal */
+	if (isatty(STDIN_FILENO) == 1)
 	{
-		print_shell_prompt();
-
-		input = read_user_input();
-
-		if (!input)
-		{
-			exit(EXIT_SUCCESS);
-		}
-
-		if (input[0] == '\0' || strcmp(input, "\n") == 0)
-		{
-			free(input);
-			continue;
-		}
-
-		if (strcmp(input, "exit\n") == 0)
-		{
-			free(input);
-			break;
-		}
-
-		source.boff  = input;
-		source.boffsize  = strlen(input);
-		source.new_post  = SOURCE_INIT_POINT;
-		parse_and_execute(&source);
-
-		free(input);
-
+		shell_interactive();
 	}
-
-	exit(EXIT_SUCCESS);
+	else
+	{
+		shell_no_interactive();
+	}
+	return (0);
 }
