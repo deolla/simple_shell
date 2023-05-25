43365e0b1a48f271309bbc697525f466525c15e3
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-05-25 04:28:33 -0700

    test.sh

---
+++ test_file/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This is a regular test file"
+echo "The shell prompt is working correctly."
+
+ls -al
+echo "ls -al command execute successfully."
+echo "Regular test file execution complete."
+
+cat i_have_no_file_like_this.txt
+
+touch i_love_c_language.c
+ls -l 
